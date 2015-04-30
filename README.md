0efa22ec1b3794d1d04eade2dac7ca8969dfe52b
Matt Holloway <hollowmatt@gmail.com>
2015-04-29 22:25:07 -0700

    a little playing around with map customization

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # iyn
 IYN: In Your Neighborhood
+
+## Sites Used for Reference
+https://developers.google.com/maps/tutorials/customizing/
+http://gmaps-samples-v3.googlecode.com/svn/trunk/pirates/
--- js/iyn.js
+++ js/iyn.js
@@ -6,5 +6,41 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(
+  	document.getElementById('legend'));
+  //  map.set('styles', [
+	//   {
+	//     featureType: 'road',
+	//     elementType: 'geometry',
+	//     stylers: [
+	//       { color: '#000000' },
+	//       { weight: 1.6 }
+	//     ]
+	//   }, {
+	//     featureType: 'road',
+	//     elementType: 'labels',
+	//     stylers: [
+	//       { saturation: -100 },
+	//       { invert_lightness: true }
+	//     ]
+	//   }, {
+	//     featureType: 'landscape',
+	//     elementType: 'geometry',
+	//     stylers: [
+	//       { hue: '#ffff00' },
+	//       { gamma: 1.4 },
+	//       { saturation: 82 },
+	//       { lightness: 96 }
+	//     ]
+	//   }, {
+	//     featureType: 'poi.school',
+	//     elementType: 'geometry',
+	//     stylers: [
+	//       { hue: '#fff700' },
+	//       { lightness: -15 },
+	//       { saturation: 99 }
+	//     ]
+	//   }
+	// ]);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
