eeefefe8f0cea6c078040037cc9f917e9cda5054
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-05-15 21:04:43 -0700

    Add some markers to the map, with some basic info

--- js/iyn.js
+++ js/iyn.js
@@ -1,18 +1,17 @@
 // This function initializes the Google Map to Mountain View, with a zoomed in view
 (function() {
-	var map, places, place;
-	var markers = [];
-	var autocomplete;
+	var map;
 	var mapfilter = { "1": "All", "2": "Restaurants", "3": "Bars" };
-	var countryRestrict = { 'country': 'us' };
 	var markers = {
 		"places" :[
-			{"type": "Bar", "name": "Tied House", "lat": "38.4", "lon": "-122.1", "content": "Makes great beer"},
-			{"type": "Restaurant", "name": "Starbucks", "lat": "37.38626", "lon": "-122.085", "content": "Makes great coffee"}
+			{"type": "Bar", "name": "Tied House", "lat": "37.394575342727734", "lon": "-122.08069235086441", "content": "Makes great beer"},
+			{"type": "Bar", "name": "Steins Beer Garden", "lat": "37.39407883199609", "lon": "-122.07989037036896", "content": "Makes great beer"},
+			{"type": "Restaurant", "name": "Starbucks", "lat": "37.38754504105412", "lon": "-122.08302319049835", "content": "Makes great coffee"},
+
 		]
 	};
 
-
+	// Setup initial page, with map centered on Mountain View, CA
 	function initialize() {
 		var mountainView =  new google.maps.LatLng(37.3860517,-122.0838511); //Mountain View, CA
 
@@ -21,24 +20,21 @@
 	    center: mountainView
 	  };
 
-	  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+	  //add marker on click
+	  google.maps.event.addListener(map, 'click', function(event) {
+	  	addMarker(event.latLng);
+	  });
 	  for (var place in markers.places) {
-			var marker = new google.maps.Marker({
-	      position: mountainView,
-	      map: map,
-	      title: markers.places[place].name
-	  	});
-	  	var infoWindow = new google.maps.InfoWindow({
-	  		content: markers.places[place].content
-	  	});
-	  	google.maps.event.addDomListener(marker, 'click', function() {
-	  		infoWindow.open(map, marker);
-	  	});
-	  	console.log(marker);
+	  	currLoc = new google.maps.LatLng(markers.places[place].lat, markers.places[place].lon);
+	  	info = markers.places[place].content;
+	  	name = markers.places[place].name;
+	  	addMarker(currLoc, info, name);
 	  }
 	}
 
+	// Filter the markers
 	function filterModel() {
 		this.markers = [
 			{ "name": "All" },
@@ -48,18 +44,31 @@
 		this.resetMarker = function() { this.chosenMarker(null)};
 	}
 
-	function addMarkers() {
-		for (var place in markers.places) {
-			console.log(markers.places[place].lat + "," + markers.places[place].lon);
-			var myLatlng = google.maps.LatLng(markers.places[place].lat, markers.places[place].lon);
+	// Add a marker
+	function addMarker(location, info, name) {
+		var marker = new google.maps.Marker({
+			position: location,
+			map: map,
+			title: name
+		});
 
-			var marker = new google.maps.Marker({
-	      position: myLatlng,
-	      map: map,
-	      title: markers.places[place].name
-	  	});
-	  	console.log(marker);
-		}
+		google.maps.event.addDomListener(marker, 'rightclick', function() {
+  		removeMarker(marker);
+  	});
+
+		var infoWindow = new google.maps.InfoWindow({
+			content: info
+		});
+
+  	google.maps.event.addDomListener(marker, 'click', function() {
+  		infoWindow.open(map, marker);
+  	})
+	}
+
+	// Remove a marker from the map
+	function removeMarker(marker) {
+		console.log(marker);
+		marker.setVisible(false);
 	}
 
 	//Do the good stuff
