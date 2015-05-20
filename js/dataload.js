102db75db441029ac175323caa437c1c82fd0483
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-05-19 21:20:09 -0700

    Fix data for the 3 cities, add appropriate Knockout selection and implement the functions to setup the map based on city.  Need to integrate the APIs now for various markers

--- js/dataload.js
+++ js/dataload.js
@@ -9,24 +9,24 @@ function getMapData() {
         "places" :[
           {
             "type": "Bar",
-            "name": "Tied House",
-            "lat": "37.394575342727734",
-            "lon": "-122.08069235086441",
-            "content": "Makes great beer"
-          },
-          {
-            "type": "Bar",
-            "name": "Steins Beer Garden",
-            "lat": "37.39407883199609",
-            "lon": "-122.07989037036896",
-            "content": "Makes great beer"
+            "name": "8th St. Wine Cellar",
+            "lat": "40.73268976628568",
+            "lon": "-73.997558131814",
+            "content": "A great place for wine"
           },
           {
             "type": "Restaurant",
-            "name": "Starbucks",
-            "lat": "37.38754504105412",
-            "lon": "-122.08302319049835",
-            "content": "Makes great coffee"
+            "name": "Peacefood",
+            "lat": "40.73296618251578",
+            "lon": "-73.99279452860355",
+            "content": "Amazing Vegan food!"
+          },
+          {
+            "type": "Restaurant",
+            "name": "Blossom du Jour",
+            "lat": "40.76097582376582",
+            "lon": "-73.99082377552986",
+            "content": "Great fast vegan take-out"
           }
         ]
       },
@@ -61,30 +61,30 @@ function getMapData() {
       },
       {
         "Name": "Pittsburgh, PA",
-        "lat": "40.73384312330757",
-        "lon": "-73.99001970728763",
-        "zoom": 15,
+        "lat": "40.441476475059055",
+        "lon": "-79.99926902353764",
+        "zoom": 14,
           "places" :[
           {
-            "type": "Bar",
-            "name": "Tied House",
-            "lat": "37.394575342727734",
-            "lon": "-122.08069235086441",
-            "content": "Makes great beer"
+            "type": "Restaurant",
+            "name": "Kaya",
+            "lat": "40.45109460901854",
+            "lon": "-79.98545363545418",
+            "content": "West Indies food sensation - vegan friendly"
           },
           {
             "type": "Bar",
-            "name": "Steins Beer Garden",
-            "lat": "37.39407883199609",
-            "lon": "-122.07989037036896",
-            "content": "Makes great beer"
+            "name": "Tonic",
+            "lat": "40.44369742276764",
+            "lon": "-79.99541569501162",
+            "content": "Makes great martinis"
           },
           {
             "type": "Restaurant",
-            "name": "Starbucks",
-            "lat": "37.38754504105412",
-            "lon": "-122.08302319049835",
-            "content": "Makes great coffee"
+            "name": "Zenith",
+            "lat": "40.426466824353994",
+            "lon": "-79.96901705861092",
+            "content": "Great Vegan Sunday Buffet"
           }
         ]
       }
--- js/iyn.js
+++ js/iyn.js
@@ -1,64 +1,96 @@
 // This function initializes the Google Map to Mountain View, with a zoomed in view
 (function() {
+	/***
+	 * Setup our variables:
+	 * - map -> this is the Google Map object
+	 * - mapfilter -> what will will filter the markers based on
+	 * - jsonData -> this will contain all of the info form our JSON object
+	 ***/
 	var map;
-	//Load data from JSON
 	var mapfilter = getMapFilters();
 	var jsonData = getMapData();
 
-	// Setup initial page, with map centered on Mountain View, CA
+
+	/***
+	 * initialize()
+	 *	 This function will Setup the initial page,
+	 *	 with map centered on the first city in the
+	 * 	 data from the JSON file
+	 ***/
 	function initialize() {
-
+		//build the map with the first city
 		buildMap(jsonData.cities[0].lat, jsonData.cities[0].lon, jsonData.cities[0].zoom);
-
-	  //add marker on click
-	  google.maps.event.addListener(map, 'click', function(event) {
-	  	addMarker(event.latLng);
-	  });
-	  // for (var place in markers.places) {
-	  // 	currLoc = new google.maps.LatLng(markers.places[place].lat, markers.places[place].lon);
-	  // 	info = markers.places[place].content;
-	  // 	name = markers.places[place].name;
-	  // 	addMarker(currLoc, info, name);
-	  // }
+	  //add markers to the map from the data
+	  addMarkers(jsonData.cities[0].places);
 	}
 
 	/***
-	 * Setup the map
+	 * buildMap(lat, lon, zoom)
+	 *	Setup the map
 	 *	- Take in the lat/lon for the map
 	 *  - setup the map options
 	 ***/
 	function buildMap(lat, lon, zoom) {
-		var defaultCity =  new google.maps.LatLng(lat,lon); //element zero
+		var currentCity =  new google.maps.LatLng(lat,lon); //city data passed in
 		var mapOptions = {
 	    zoom: zoom,
-	    center: defaultCity
+	    center: currentCity
 	  };
 
 	  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	  //TODO: Remove this event listener once we have all the markers we need in our
+	  //			JSON file
+	  google.maps.event.addListener(map, 'click', function(event) {
+	  	addMarker(event.latLng);
+	  });
 	}
+
 	/***
-	 * This is the KnockoutJS model for the page
+	 * filterModel()
+	 *	This is the KnockoutJS model for the page
 	 *	 - Filter the markers
 	 *	 - load the City selector
 	 ***/
 	function filterModel() {
+		var self = this;
+
 		//Filters for the markers
-		this.markers = [
+		self.markers = [
 			{ "name": "All" },
 			{ "name": "Restaurants" },
 			{ "name": "Bars" }];
-		this.chosenMarker = ko.observable();
-		this.resetMarker = function() { this.chosenMarker(null)};
+		self.chosenMarker = ko.observable();
+		self.resetMarker = function() { self.chosenMarker(null)};
 
 		//Load the city selector
-		console.log("Put out the cities");
-		console.log(jsonData);
-		this.cities = jsonData.cities;
-		this.chosenCity = ko.observable();
-		this.setMap = function() { console.log(this)};
+		self.cities = jsonData.cities;
+		self.chosenCity = ko.observable();
+		self.chosenCity.subscribe(function() {
+			buildMap(self.chosenCity().lat, self.chosenCity().lon, self.chosenCity().zoom);
+			addMarkers(self.chosenCity().places);
+		});
 	}
 
-	// Add a marker
+	/***
+	 * addMarkers(markers)
+	 * 	This takes in an array of places from the JSON data, and will
+	 *	call the addMarker function for each place
+	 ***/
+	function addMarkers(markers) {
+		for (var location in markers) {
+			loc = new google.maps.LatLng(markers[location].lat, markers[location].lon);
+			addMarker(loc, markers[location].content, markers[location].name);
+		}
+	}
+
+	/***
+	 * addMarker(location, info, name)
+	 *	This will add a marker to the map
+	 *	 - location is where on the map
+	 *	 - info is what shows up in the box when you click the marker
+	 *	 - name is the name of the location
+	 ***/
 	function addMarker(location, info, name) {
 		var marker = new google.maps.Marker({
 			position: location,
@@ -79,7 +111,11 @@
   	})
 	}
 
-	// Remove a marker from the map
+	/***
+	 * removeMarker(marker)
+	 * 	This will remove a marker from the map
+	 *	TODO: Remove this function, used for map setup of JSON data
+	 ***/
 	function removeMarker(marker) {
 		console.log(marker);
 		marker.setVisible(false);
