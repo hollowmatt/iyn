5c72fe98f7e64cad6996695d1184d8e42620cd44
Matthew Holloway <matthew.holloway@logicwerx.com>
2015-06-01 21:19:37 -0700

    Fix the yelp API to only add content to an infobox once, also create the left list, and add click function to it to activate the markers

--- js/iyn.js
+++ js/iyn.js
@@ -89,6 +89,7 @@
 		self.chosenCity.subscribe(function() {
 			buildMap(self.chosenCity().lat, self.chosenCity().lon, self.chosenCity().zoom);
 			setTitle(self.chosenCity().Name);
+			removeListItems();
 			addMarkers(self.chosenCity().places);
 		});
 	}
@@ -103,6 +104,7 @@
 			loc = new google.maps.LatLng(markers[location].lat, markers[location].lon);
 			addMarker(loc, markers[location].content, markers[location].yelp, markers[location].name, markers[location].title);
 		}
+		setListStyle();
 	}
 
 	/***
@@ -119,8 +121,6 @@
 			title: title
 		});
 
-
-
 		google.maps.event.addDomListener(marker, 'rightclick', function() {
   		removeMarker(marker);
   	});
@@ -135,7 +135,9 @@
   		if (yelp) {
 				getYelpInfo(yelp, name);
 			}
-  	})
+  	});
+
+  	addListItem(title, name, marker);
 	}
 
 	/***
@@ -181,9 +183,14 @@
 		}).success(function(data) {
 			var busPhone = data.businesses[0].display_phone;
 			var busRating = data.businesses[0].rating_img_url
-			$yelpElem.append("<div class='infoyelplogo'><img src='images/yelp_logo_75x38.png'></div>");
-			$yelpElem.append("<p>Phone number: " + busPhone + "</p>");
-			$yelpElem.append("<p><img src='" + busRating + "'>");
+
+			//check to see if we already added the yelp stuff
+			var exists = $('#' + data.businesses[0].id).length;
+			if(exists < 1) {
+				$yelpElem.append("<div class='infoyelplogo' id=" + data.businesses[0].id + "><img src='images/yelp_logo_75x38.png'></div>");
+				$yelpElem.append("<p>Phone number: " + busPhone + "</p>");
+				$yelpElem.append("<p><img src='" + busRating + "'>");
+			}
 		}).error(function(e) {
 			console.log("an error has occured");
 			console.log(e);
@@ -198,6 +205,41 @@
 	function removeMarker(marker) {
 		console.log(marker);
 		marker.setVisible(false);
+	}
+
+	/***
+	 * addListItem(name, id)
+	 * This will populate the list of results on the left side, and be clickable
+	 * to allow invoking the infobox on the marker
+	 */
+	function addListItem(name, id, marker) {
+		item = "<li id='" + id + "-li'>" + name + "</li>"
+		$('#filteredResults').append(item);
+		$('#' + id +"-li").click(function() {
+			console.log("click");
+			google.maps.event.trigger(marker, 'click');
+		});
+	}
+
+	/***
+	 * setListStyle()
+	 * Set a hover style for the list
+	 ***/
+	function setListStyle() {
+		$('#filteredResults li').mouseover(function() {
+			$(this).addClass('zebraHover');
+		});
+		$('#filteredResults li').mouseout(function() {
+			$(this).removeClass('zebraHover');
+		});
+	}
+
+	/***
+	 * removeListItems()
+	 *	clear the list
+	 ***/
+	function removeListItems() {
+		$('#filteredResults').empty();
 	}
 
 	/***
